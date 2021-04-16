e114d370ab0bbb4f8d99945c9f9edb1b6ab779bc
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-04-16 20:30:06 +0200

    [master] Project setup

---
+++ Conference Rooms/Conference Rooms/Conference_RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conference_RoomsApp.swift
+//  Conference Rooms
+//
+//  Created by Giuseppe Diciolla on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Conference_RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Conference Rooms/Conference Rooms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Conference Rooms
+//
+//  Created by Giuseppe Diciolla on 16/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
