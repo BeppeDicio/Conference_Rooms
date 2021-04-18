11d3e6e2427bbf7cd493c4cfc5db571683a25efa
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-04-18 11:35:53 +0200

    [master] design of detail view

--- Conference Rooms/Conference Rooms/RoomDetailView.swift
+++ Conference Rooms/Conference Rooms/RoomDetailView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct RoomDetailView: View {
     let room: Room
+    @State private var zoomed = false
     
     var body: some View {
-        Image(room.imageName)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
+        ZStack(alignment: .topLeading) {
+            Image(room.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation{ self.zoomed.toggle() }
+                    
+                }
+                .navigationBarTitle(Text(room.name), displayMode: .inline)
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            
+            if room.hasVideo && !zoomed {
+                Image(systemName: "video.fill")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .transition(.move(edge: .leading))
+            }
+        }
     }
 }
 
 #if DEBUG
 struct RoomDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        RoomDetailView(room: testData[0])
+        Group {
+            NavigationView {RoomDetailView(room: testData[0])}
+            NavigationView {RoomDetailView(room: testData[1])}
+        }
     }
 }
 #endif
