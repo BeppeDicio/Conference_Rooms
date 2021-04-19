d2b20c97b58713746d46e0105a9eb5a1bb11834f
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-04-19 10:36:45 +0200

    [master] added list interaction features

--- Conference Rooms/Conference Rooms/ContentView.swift
+++ Conference Rooms/Conference Rooms/ContentView.swift
@@ -8,23 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var rooms: [Room] = []
+    @ObservedObject var store = RoomStore()
     
     
     var body: some View {
         NavigationView {
-            List(rooms) { room in
-                RoomCell(room: room)
+            List {
+                Section {
+                    Button(action: addRoom){
+                        Text("Add Room")
+                    }
+                }
+                Section {
+                    ForEach(store.rooms) { room in
+                        RoomCell(room: room)
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
+                }
             }
             .navigationBarTitle(Text("Meeting Rooms"))
+            .navigationBarItems(trailing: EditButton())
+            .listStyle(GroupedListStyle())
         }
+    }
+    
+    func addRoom() {
+        store.rooms.append(Room(name: "Hall 2", capacity: 2000))
+    }
+    
+    func delete(at offsets: IndexSet) {
+        store.rooms.remove(atOffsets: offsets)
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        store.rooms.move(fromOffsets: source, toOffset: destination)
     }
 }
 
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(rooms: testData)
+        Group {
+            ContentView(store: RoomStore(rooms: testData))
+            ContentView(store: RoomStore(rooms: testData))
+                .environment(\.sizeCategory, .extraExtraExtraLarge)
+            ContentView(store: RoomStore(rooms: testData))
+                .environment(\.colorScheme, .dark)
+        }
     }
 }
 #endif
